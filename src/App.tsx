e2be8e124bcf7b1bcd030957450d532927793b11
Theekshana Wijerathne <theekshanawijerathne101@gmail.com>
2025-08-10 08:25:57 +0530

    feat: Add About and NotFound pages; implement Layout component with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,30 @@
 import './App.css'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import LandingPage from './pages/LandingPage';
 import CreateQuiz from './pages/quiz/createquiz';
 import { ThemeDemo } from './theme';
 import  LoginPage from './pages/LoginPage';
+import NotFound from './pages/404';
+import About from './pages/About';
+import Layout from './components/Layout';
 
-function About() {
-  return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the about page.</p>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/create-quiz">Create Quiz</Link> | <Link to="/theme-demo">Theme Demo</Link>
-      </nav>
-    </div>
-  )
-}
-
-function NotFound() {
-  return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The requested page could not be found.</p>
-      <nav>
-        <Link to="/">Go back to home</Link>
-      </nav>
-    </div>
-  )
-}
 
 function App() {
   
   return (
     <Routes>
+      
       <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/create-quiz" element={<CreateQuiz />} />
-      <Route path="/theme-demo" element={<ThemeDemo />} />
       <Route path="/login" element={<LoginPage />} />
+
+      <Route element={<Layout />}>
+        <Route path="about" element={<About />} />
+        <Route path="create-quiz" element={<CreateQuiz />} />
+        <Route path="theme-demo" element={<ThemeDemo />} />
+      </Route>
+      
       <Route path="*" element={<NotFound />} />
+      
     </Routes>
     )
 }
